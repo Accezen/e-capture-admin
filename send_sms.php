85b0f2df44281cfe3eb88bcd73a9984a7be19716
Jan Patrick Urbano <janpatrickdelacruzurbano@gmail.com>
2025-02-19 16:37:03 +0800

    remove api secrets

--- send_sms.php
+++ send_sms.php
@@ -1,14 +1,15 @@
 <?php
 session_start();
 require __DIR__ . '/vendor/autoload.php';
+
 use Twilio\Rest\Client;
 
 $phone = $_POST['phone'];
 
-$account_sid = 'ACba59bcd1bc901d7102ffd95b0465cfe5';
-$auth_token = 'bab13a25e5ec156af62ac349d8d6d917';
+$account_sid = '#';
+$auth_token = '#';
 
-$twilio_number = "+19285758460";
+$twilio_number = "#";
 
 $client = new Client($account_sid, $auth_token);
 
@@ -19,4 +20,3 @@ $client->messages->create(
         'body' => 'We have received your report and we will coordinate this with the authority. We appreciate your concern and will work to resolve the issue as soon as possible. Thank you for bringing this to our attention.'
     )
 );
-?>
